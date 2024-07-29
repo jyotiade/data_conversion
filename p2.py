70e42742795f60b2747a1d6bf4f1180249b175e8
jyoti ade <jyotiade287@gmail.com>
2024-07-29 23:55:00 +0530

    new

--- p2.py
+++ p2.py
@@ -3,3 +3,5 @@ import json
 
 data='{"Name": "jyoti", "quali": "b.tech", "city_bhopal": true, "belongs": false, "activity": null}'
 print(json.loads(data))
+data='{"Name": "avi", "quali": "b.tech", "city_balaghat": true, "belongs": false, "activity": null}'
+print(json.loads(data))
